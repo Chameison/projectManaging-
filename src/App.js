c80bc5de9b25cc12b1dbb63257dac32751e25c47
Chameison Araújo <chameison.acre@gmail.com>
2023-03-01 02:04:16 -0300

    19 - navbar e footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import Home from './components/pages/Home';
+import Contact from './components/Contact';
+import Company from './components/pages/Company';
+import NewProject from './components/pages/NewProject';
+
+
+//Layoyt
+import Container from './components/layouts/Container';
+import Navbar from './components/layouts/Navbar';
+import Footer from './components/layouts/Footer';
+import Projects from './components/pages/Projects';
 
 function App() {
   return (
-    <div className="App">
-     <p>Projetos</p>
-    </div>
+    <Router>
+      <Navbar />
+      <Container customClass="min-height">
+
+      <Routes>
+          <Route exact path="/" element={ <Home /> } />
+          <Route exact path="/company" element={ <Company /> } />
+          <Route exact path="/contact" element={ <Contact /> } />
+          <Route exact path="/projects" element={ <Projects /> } />
+
+          <Route exact path="/newproject" element={ <NewProject /> } />
+      </Routes>
+      </Container>
+      <Footer />
+      </Router>
   );
 }
 
