069320ec5a6e33d5a0a4a384b0ff27997fcf6884
Chameison Araújo <chameison.acre@gmail.com>
2023-03-05 23:59:34 -0300

    atualizando o projeto e messagens - 32

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -3,16 +3,21 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Loading from "../layouts/Loading";
 import Container from "../layouts/Container";
+import Message from "../layouts/Message"
+//ele permite que possamos customizar o submit, e fazer um adição ou atualização
+import ProjectForm from "../projects/ProjectForm";
+
 function Project() {
   const { id } = useParams();
   const [project, setProject] = useState([]);
-
+  const [message, setMessage] = useState()
+  const [type, setType] = useState()
   //vamos criar um styate respnsavel por mostrar ou nao o projeto
 
-  const [showProjectForm, setShowProjectForm] = useState(false)
+  const [showProjectForm, setShowProjectForm] = useState(false);
   //vmos criar uma funcao anonimas
   useEffect(() => {
-    setTimeout (() => {
+    setTimeout(() => {
       fetch(`http://localhost:5000/projects/${id}`, {
         method: "GET",
         headers: {
@@ -21,29 +26,55 @@ function Project() {
       })
         .then((rest) => rest.json())
         .then((data) => {
-          setProject(data)
+          setProject(data);
         })
         .catch((err) => console.log(err));
-    }, 3000) //loading para carregar o projeto
+    }, 3000); //loading para carregar o projeto
   }, [id]); //depois de finalizarmos essa requisição termos acesso de forma dinamica aos dados do projeto que foi clicado
 
+  function editPost(project) {
+    //budget Validation
+    if (project.budget < project.cost) {
+      setMessage('O orçamento não pode ser menor que o custo do projeto')
+      setType('error')
+      return false
+    }
+    fetch(`http://localhost:5000/projects/${project.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(project),
+    })
+      .then((res => res.json()))
+      .then((data) => {
+        setProject(data) //estamos mudadno o estado de fato
+        setShowProjectForm(false)
+        setMessage('Projeto Atualizado Com Sucesso')
+        setType('success')        //vamos passar os dados atualizados
+      })
+      .catch((err) => console.log(err));
+    //ja estamos alteradno o estado do objeto
+  }
+
   function toggleProjectForm() {
-    setShowProjectForm(!showProjectForm)
+    setShowProjectForm(!showProjectForm);
   }
 
   return (
     <>
       {project.name ? (
-        <div className={styles.project_details}> 
+        <div className={styles.project_details}>
           <Container customClass="column">
+
+            {/* Mensagem */}
+            {message && <Message  type={type} msg={message}/>}
             <div className={styles.details_container}>
               <h1>Projeto {project.name}</h1>
-              <button className={styles.btn} onClick={toggleProjectForm}>{!showProjectForm ? 'EditarProjeto': 'Fechar'}</button> 
+              <button className={styles.btn} onClick={toggleProjectForm}>
+                {!showProjectForm ? "Editar Projeto" : "Fechar"}
+              </button>
               {!showProjectForm ? (
-                <div className={styles.project_info}>
-                  <p>Project Form</p>
-                </div>
-              ): (
                 <div className={styles.project_info}>
                   <p>
                     <span>Categoria: </span> {project.category.name}
@@ -55,14 +86,22 @@ function Project() {
                     <span>Total Utilizado: </span> R${project.cost}
                   </p>
                 </div>
+              ) : (
+                <div className={styles.project_info}>
+                  <ProjectForm
+                    handleSubmit={editPost}
+                    btnText="Concluir Edição"
+                    projectData={project}
+                  />
+                </div>
               )}
             </div>
           </Container>
         </div>
-      ): (
+      ) : (
         <Loading />
       )}
     </>
-  )
+  );
 }
 export default Project;
